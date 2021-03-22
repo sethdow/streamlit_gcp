371a10c4f52e4194504e22b17c33d771f3447a2b
sdow <seth.dow@gmail.com>
2021-03-22 15:09:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ secrets = secretmanager.SecretManagerServiceClient()
 # This is passed in as an environ by default?
 PROJECT_ID = os.environ.get("PROJECTID")
 
-SECRET_VALUE = secrets.access_secret_version(request={"name": "projects/"+PROJECT_ID+"/secrets/example-secrety/versions/1"}).payload.data.decode("utf-8")
+SECRET_VALUE = secrets.access_secret_version(request={"name": "projects/"+PROJECT_ID+"/secrets/example-secret/versions/1"}).payload.data.decode("utf-8")
 
 st.write('This is your steamlit app')
 st.write('EXAMPLE SECRET: ' + SECRET_VALUE)
