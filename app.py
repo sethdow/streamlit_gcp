6660bf4fe7121b49f3fd768b7cbf4fbd19e1a87c
sdow <seth.dow@gmail.com>
2021-03-22 15:09:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import streamlit as st
-import numpy as np
-import pandas as pd 
+import os
+from google.cloud import secretmanager
 
-def main():
 
-    st.write("These are the words")
+secrets = secretmanager.SecretManagerServiceClient()
 
-if __name__ == '__main__':
-    main()
+# This is passed in as an environ by default?
+PROJECT_ID = os.environ.get("PROJECTID")
+
+SECRET_VALUE = secrets.access_secret_version(request={"name": "projects/"+PROJECT_ID+"/secrets/example-secrety/versions/1"}).payload.data.decode("utf-8")
+
+st.write('This is your steamlit app')
+st.write('EXAMPLE SECRET: ' + SECRET_VALUE)
